762c0a805924d7d07f6355e92ec82e54c5edfe3d
William Ife <iamife@williams-mbp.home>
2020-05-26 08:56:36 +0100

    Add string_reverse to project

---
+++ string_reverse.py
@@ -0,0 +1,4 @@
+#3. Write a python function to reverse a string, then ask the users for a string to test it.
+
+word = input("Enter a word you would like the Quiz Master to reverse: ")
+print(word[::-1])
